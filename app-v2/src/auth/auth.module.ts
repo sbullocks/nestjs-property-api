c8e8ec25dcca659efdcb449fb25eac4f66515715
Stephen <sbullocks@gmail.com>
2026-06-17 19:28:42 -0400

    updated secret in useFactory

--- app-v2/src/auth/auth.module.ts
+++ app-v2/src/auth/auth.module.ts
@@ -1,24 +1,25 @@
 import { Module } from '@nestjs/common';
-  import { AuthService } from './auth.service';
-  import { AuthController } from './auth.controller';
-  import { PassportModule } from '@nestjs/passport';
-  import { JwtModule } from '@nestjs/jwt';
-  import { JwtStrategy } from './jwt.strategy';
-  import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
-  @Module({
-    imports: [
-      PassportModule,
-      JwtModule.registerAsync({
-        imports: [ConfigModule],
-        inject: [ConfigService],
-        useFactory: (config: ConfigService) => ({
-          secret: process.env.JWT_SECRET,
-          signOptions: { expiresIn: '7d' },
-        })
+@Module({
+  imports: [
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        // secret: process.env.JWT_SECRET,
+        secret: config.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '7d' },
       }),
-    ],
-    providers: [AuthService, JwtStrategy],
-    controllers: [AuthController],
-  })
-  export class AuthModule {}
+    }),
+  ],
+  providers: [AuthService, JwtStrategy],
+  controllers: [AuthController],
+})
+export class AuthModule {}
